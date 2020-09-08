0f90120bcb3c298d45a40edf8b21e7fec51804e8
genrihssaruhanovs <61009362+genrihssaruhanovs@users.noreply.github.com>
2020-09-08 22:32:19 +0300

    Add files via upload

---
+++ Poker/src/com/company/pokerTables/TexasHoldemPokerTable.java
@@ -0,0 +1,22 @@
+package com.company.pokerTables;
+
+import com.company.CardSet;
+import com.company.EvaluatedHand;
+import com.company.combinations.CombinationFactory;
+import com.company.enums.PokerTableTypes;
+
+import java.util.Collections;
+
+public class TexasHoldemPokerTable extends HoldemPokerTable {
+    public TexasHoldemPokerTable(String inputString) {
+        super(inputString, PokerTableTypes.TEXAS);
+    }
+
+    @Override
+    public void evaluateTable() {
+        for (CardSet hand : hands) {
+            evaluatedHands.add(new EvaluatedHand(hand, CombinationFactory.getBestCombination(board, hand)));
+        }
+        Collections.sort(evaluatedHands);
+    }
+}
