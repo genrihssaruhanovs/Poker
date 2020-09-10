d06fa3a5da62a8da084dcebfa90829afb088eb3d
Genrihs Saruhanovs <genrihs.saruhanovs@gmail.com>
2020-09-10 17:40:24 +0300

    New syntax reversal

--- src/com/company/combinations/decorator/CombinationCreatorFactory.java
+++ src/com/company/combinations/decorator/CombinationCreatorFactory.java
@@ -5,16 +5,27 @@ import com.company.enums.Combinations;
 
 public class CombinationCreatorFactory {
     public static CombinationCreator getInstance(Combinations combination, CombinationCreator combinationCreator, HandCombinations handCombinations) {
-        return switch (combination) {
-            case HIGH_CARD -> new HighCardCreator(combinationCreator, handCombinations);
-            case PAIR -> new PairCreator(combinationCreator, handCombinations);
-            case TWO_PAIRS -> new TwoPairsCreator(combinationCreator, handCombinations);
-            case THREE_OF_A_KIND -> new ThreeOfAKindCreator(combinationCreator, handCombinations);
-            case STRAIGHT -> new StraightCreator(combinationCreator, handCombinations);
-            case FLUSH -> new FlushCreator(combinationCreator, handCombinations);
-            case FULL_HOUSE -> new FullHouseCreator(combinationCreator, handCombinations);
-            case FOUR_OF_A_KIND -> new FourOfAKindCreator(combinationCreator, handCombinations);
-            case STRAIGHT_FLUSH -> new StraightFlushCreator(combinationCreator, handCombinations);
-        };
+        switch (combination) {
+            case HIGH_CARD:
+                return new HighCardCreator(combinationCreator, handCombinations);
+            case PAIR:
+                return new PairCreator(combinationCreator, handCombinations);
+            case TWO_PAIRS:
+                return new TwoPairsCreator(combinationCreator, handCombinations);
+            case THREE_OF_A_KIND:
+                return new ThreeOfAKindCreator(combinationCreator, handCombinations);
+            case STRAIGHT:
+                return new StraightCreator(combinationCreator, handCombinations);
+            case FLUSH:
+                return new FlushCreator(combinationCreator, handCombinations);
+            case FULL_HOUSE:
+                return new FullHouseCreator(combinationCreator, handCombinations);
+            case FOUR_OF_A_KIND:
+                return new FourOfAKindCreator(combinationCreator, handCombinations);
+            case STRAIGHT_FLUSH:
+                return new StraightFlushCreator(combinationCreator, handCombinations);
+            default:
+                return null;
+        }
     }
 }
