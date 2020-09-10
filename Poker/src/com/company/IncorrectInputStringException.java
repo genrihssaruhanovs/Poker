df68f5c50d6497bdfa3affea025a5dad4452e460
Genrihs Saruhanovs <genrihs.saruhanovs@gmail.com>
2020-09-10 12:35:27 +0300

    Multiple Three of a Kind bugfix Project re-upload

--- Poker/src/com/company/IncorrectInputStringException.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class IncorrectInputStringException extends RuntimeException {
-    public IncorrectInputStringException(String message) {
-        super("Wrong input string: " + message);
-    }
-}
